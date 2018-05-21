dca05eed0173b6ac7914d4155cc6ad09b4b79124
Zheng Han <zhenghan@catail.cn>
2018-05-22 00:34:35 +0800

    setup react router with two dummy page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import AboutPage from "./components/pages/AboutPage";
 
-const App = () => <h1>My Portfolio</h1>;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/" component={HomePage} />
+    </Switch>
+  </Router>
+);
 
 export default App;
