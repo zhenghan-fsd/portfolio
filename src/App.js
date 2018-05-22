b0256fc77791a5b552cdf3ddb6f3447b159907ee
Zheng Han <zhenghan@catail.cn>
2018-05-22 08:57:42 +0800

    dummy contact page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import AboutPage from "./components/pages/AboutPage";
+import ContactPage from "./components/pages/ContactPage";
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/contact" component={ContactPage} />
       <Route exact path="/" component={HomePage} />
     </Switch>
   </Router>
